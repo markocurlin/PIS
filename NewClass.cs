81a51746923da18cb5c609b92ce08c5290c6e4b9
markocurlin <marko.curlin22@gmail.com>
2022-03-15 18:23:46 +0100

    Added new class

--- NewClass.cs
+++ NewClass.cs
@@ -1,10 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ConsoleApp2
 {
     class NewClass
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Novo");
+        }
     }
 }
